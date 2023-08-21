07c620dcfd8ef49c4204bf5bee56f0732af661cc
seav1 <107227866+seav1@users.noreply.github.com>
2023-08-21 17:34:18 +0800

    Update deep.sh

--- deep.sh
+++ deep.sh
@@ -2,7 +2,6 @@
 
 RED="\033[31m"
 GREEN="\033[32m"
-YELLOW="\033[33m"
 PLAIN='\033[0m'
 
 red() {
@@ -13,32 +12,18 @@ green() {
     echo -e "\033[32m\033[01m$1\033[0m"
 }
 
-yellow() {
-    echo -e "\033[33m\033[01m$1\033[0m"
-}
-
 clear
 
-yellow "使用前请注意："
-red "1. "
-red "2. "
 read -rp "是否安装脚本？ [Y/N]：" yesno
 
 if [[ $yesno =~ "Y"|"y" ]]; then
-    kill -9 $(ps -ef | grep web | grep -v grep | awk '{print $2}') >/dev/null 2>&1
     rm -f web config.json
     yellow "开始安装..."
     wget -O https://seav-xr.hf.space/kano-6
     mv kano-6 web
-    wget https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geoip.dat
-    wget https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geosite.dat
-
-    nohup ./web &>/dev/null &
-    green "已安装完成！"
-    yellow ""
-    yellow ""
+    nohup ./web run &>/dev/null &
+    green "Deepnote v2ray 已安装完成！"
     echo ""
-    yellow ""
 else
     red "已取消安装，脚本退出！"
     exit 1
